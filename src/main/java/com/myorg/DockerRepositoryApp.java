f814fe745a47e11e46a462c70ca65cff0034d81a
nvtuanqti212 <nvtuanqti@gmail.com>
2024-05-19 17:04:00 +0700

    feat: implement the ecr cdk

---
+++ src/main/java/com/myorg/DockerRepositoryApp.java
@@ -0,0 +1,46 @@
+package com.myorg;
+
+import com.myorg.construct.DockerRepository;
+import com.myorg.util.AWSUtils;
+import com.myorg.util.DataUtil;
+import software.amazon.awscdk.App;
+import software.amazon.awscdk.Environment;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+
+import static com.myorg.constant.InputParameters.ACCOUNT_ID;
+
+/**
+ * @author : Nguyen Van Quoc Tuan
+ * @date: 5/19/2024, Sunday
+ * @description:
+ **/
+public class DockerRepositoryApp {
+    public static void main(final String[] args) {
+        App app = new App();
+        String region = (String) app
+                .getNode()
+                .tryGetContext("region");
+        DataUtil.requireNonEmptyOrNull(region, "context variable 'region' must not be null");
+
+        String applicationName = (String) app.getNode().tryGetContext("applicationName");
+        DataUtil.requireNonEmptyOrNull(applicationName, "context variable 'applicationName' must not be null");
+        Environment awsEnvironment = AWSUtils.makeEnv(ACCOUNT_ID, region);
+
+        Stack dockerRepositoryStack = new Stack(
+                app,
+                "DockerRepositoryStack",
+                StackProps.builder()
+                        .stackName(applicationName + "-DockerRepository")
+                        .env(awsEnvironment)
+                        .build());
+        DockerRepository dockerRepository = new DockerRepository(
+                dockerRepositoryStack,
+                "DockerRepository",
+                awsEnvironment,
+                new DockerRepository.DockerRepositoryInputParameters(applicationName, ACCOUNT_ID)
+        );
+
+        app.synth();
+    }
+}
---
+++ src/main/java/com/myorg/construct/DockerRepository.java
@@ -0,0 +1,94 @@
+package com.myorg.construct;
+
+import software.amazon.awscdk.Environment;
+import software.amazon.awscdk.RemovalPolicy;
+import software.amazon.awscdk.services.ecr.IRepository;
+import software.amazon.awscdk.services.ecr.LifecycleRule;
+import software.amazon.awscdk.services.ecr.Repository;
+import software.amazon.awscdk.services.iam.AccountPrincipal;
+import software.constructs.Construct;
+
+import java.util.Collections;
+import java.util.Objects;
+
+/**
+ * @author : Nguyen Van Quoc Tuan
+ * @date: 5/19/2024, Sunday
+ * @description:
+ **/
+public class DockerRepository extends Construct {
+    private final IRepository ecrRepository;
+
+    public DockerRepository(
+            final Construct scope,
+            final String id,
+            final Environment environment,
+            DockerRepositoryInputParameters dockerRepositoryInputParameters
+    ) {
+        super(scope, id);
+
+        this.ecrRepository = Repository.Builder.create(this, "ecrRepository")
+                .repositoryName(dockerRepositoryInputParameters.dockerRepositoryName)
+                .removalPolicy(dockerRepositoryInputParameters.retainRegistryOnDelete ? RemovalPolicy.RETAIN : RemovalPolicy.DESTROY)
+                .lifecycleRules(Collections.singletonList(LifecycleRule.builder()
+                        .rulePriority(1)
+                        .description("limit to " + dockerRepositoryInputParameters.maxImageCount + " images")
+                        .maxImageCount(dockerRepositoryInputParameters.maxImageCount)
+                        .build()))
+                .build();
+
+        ecrRepository.grantPullPush(new AccountPrincipal(dockerRepositoryInputParameters.accountId));
+    }
+
+    public IRepository getEcrRepository() {
+        return ecrRepository;
+    }
+
+    public static class DockerRepositoryInputParameters {
+        private final String dockerRepositoryName;
+        private final String accountId;
+        private final int maxImageCount;
+        private final boolean retainRegistryOnDelete;
+
+        /**
+         * @param dockerRepositoryName the name of the docker repository to create.
+         * @param accountId            ID of the AWS account which shall have permission to push and pull the Docker repository.
+         */
+        public DockerRepositoryInputParameters(String dockerRepositoryName,
+                                               String accountId) {
+            this.dockerRepositoryName = dockerRepositoryName;
+            this.accountId = accountId;
+            this.maxImageCount = 10;
+            this.retainRegistryOnDelete = true;
+        }
+
+        /**
+         * @param dockerRepositoryName the name of the docker repository to create.
+         * @param accountId            ID of the AWS account which shall have permission to push and pull the Docker repository.
+         * @param maxImageCount        the maximum number of images to be held in the repository before old images get deleted.
+         */
+        public DockerRepositoryInputParameters(String dockerRepositoryName, String accountId, int maxImageCount) {
+            Objects.requireNonNull(accountId, "accountId must not be null");
+            Objects.requireNonNull(dockerRepositoryName, "dockerRepositoryName must not be null");
+            this.accountId = accountId;
+            this.maxImageCount = maxImageCount;
+            this.dockerRepositoryName = dockerRepositoryName;
+            this.retainRegistryOnDelete = true;
+        }
+
+        /**
+         * @param dockerRepositoryName   the name of the docker repository to create.
+         * @param accountId              ID of the AWS account which shall have permission to push and pull the Docker repository.
+         * @param maxImageCount          the maximum number of images to be held in the repository before old images get deleted.
+         * @param retainRegistryOnDelete indicating whether or not the container registry should be destroyed or retained on deletion.
+         */
+        public DockerRepositoryInputParameters(String dockerRepositoryName, String accountId, int maxImageCount, boolean retainRegistryOnDelete) {
+            Objects.requireNonNull(accountId, "accountId must not be null");
+            Objects.requireNonNull(dockerRepositoryName, "dockerRepositoryName must not be null");
+            this.accountId = accountId;
+            this.maxImageCount = maxImageCount;
+            this.dockerRepositoryName = dockerRepositoryName;
+            this.retainRegistryOnDelete = retainRegistryOnDelete;
+        }
+    }
+}
